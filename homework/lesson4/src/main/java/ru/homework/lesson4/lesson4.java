78656e2ee1657948a8ae175d72858e60e806863d
matveev-ra <ramzez1@gmail.com>
2022-04-13 11:59:09 +0300

    13042022_1258

--- homework/lesson4/src/main/java/ru/homework/lesson4/lesson4.java
+++ homework/lesson4/src/main/java/ru/homework/lesson4/lesson4.java
@@ -1,10 +1,24 @@
 package ru.homework.lesson4;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class lesson4 {
 
 
+    static final char DOT_HUMAN = 'X'; // Фишка игрока - человек
+    static final char DOT_AI = '0'; // Фишка игрока - компьютер
+    static final char DOT_EMPTY = '•'; // Признак пустого поля
+    static final int WIN_COUNT = 3; // Выигрышная комбинация
 
-    
+    static final Scanner scanner = new Scanner(System.in);
+    static final Random random = new Random();
+
+    static char[][] field; // Двумерный массив, хранит текущее состояние игрового поля
+    static int fieldSizeX; // Размерность игрового поля
+    static int fieldSizeY; // Размерность игрового поля
+
+
     public static void main(String[] args) {
 
     }
